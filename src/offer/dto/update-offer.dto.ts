6328ab2fb65a6ab5a9b215a34097e6c5885cec31
EmiCon97 <derleuchtturm97@gmail.com>
2023-01-30 18:39:03 -0300

    taking an offer now changes the offer status to TAKEN

--- src/offer/dto/update-offer.dto.ts
+++ src/offer/dto/update-offer.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsIn, IsOptional } from 'class-validator';
 import { CreateOfferDto } from './create-offer.dto';
+import { statusList } from './../types/status.type';
 
-export class UpdateOfferDto extends PartialType(CreateOfferDto) {}
+export class UpdateOfferDto extends PartialType(CreateOfferDto) {
+  @IsOptional()
+  @IsIn(statusList)
+  status: string;
+}
